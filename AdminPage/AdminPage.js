79a966c9354a0f8e39e0c9c78727759c89dc1664
Daaawin <drwnstq@gmail.com>
2025-09-22 18:19:43 +0800

    AdminPage updated with manipulation of database

--- AdminPage/AdminPage.js
+++
@@ -1,5 +0,0 @@
-const logout = document.getElementById("btn-logout");
-logout.addEventListener("click", function (event) {
-    event.preventDefault();
-    window.location.href = "/LOGIN/LoginPage.html";
-});
